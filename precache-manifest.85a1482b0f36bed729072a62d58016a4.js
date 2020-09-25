dea1f21634ba43a576d3b78b43229f1343a42daa
Shidesin <shidesin@gmail.com>
2020-09-25 21:09:11 +0300

    Updates

---
+++ precache-manifest.85a1482b0f36bed729072a62d58016a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db211d400befa60722c6b3800e4b48ab",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "d737b137b12bf4ee4573",
+    "url": "/TodoList/static/css/main.8c28df87.chunk.css"
+  },
+  {
+    "revision": "01b92e48686420105b27",
+    "url": "/TodoList/static/js/2.28e9fc9d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TodoList/static/js/2.28e9fc9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d737b137b12bf4ee4573",
+    "url": "/TodoList/static/js/main.8bee8327.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
